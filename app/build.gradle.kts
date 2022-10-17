411c5be3c5f80ad8d7240e1b1397e59767187a16
Lmh170 <97251582+Lmh170@users.noreply.github.com>
2022-10-17 11:02:51 +0200

    Upgrade dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,17 +39,14 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.5.0")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference:1.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-
-    //ExifInterface
-    implementation("androidx.exifinterface:exifinterface:1.3.3")
 
     // RecyclerView selection
     implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
@@ -59,9 +56,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
 
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.1")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.1")
-    androidTestImplementation("androidx.navigation:navigation-testing:2.5.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.2")
+    androidTestImplementation("androidx.navigation:navigation-testing:2.5.2")
 
     // Glide
     implementation("com.github.bumptech.glide:glide:4.14.2")
